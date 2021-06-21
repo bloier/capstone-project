513227f07ec6ad130e47c86f2fee3883f24de601
bloier <airinaight@gmail.com>
2021-06-21 14:45:31 +0200

    Update routing to new game page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import GamePage from "./pages/GamePage";
 
 export default function App() {
   return (
-      <Router>
-        <Switch>
-          <Route path={["/", "/home"]}>
-            <HomePage />
-          </Route>
-        </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        <Route path={["/", "/home"]} exact>
+          <HomePage />
+        </Route>
+        <Route path={"/games/wordflow"} exact>
+          <GamePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,17 +1,23 @@
 import styled from "styled-components/macro";
 import CardWordOfTheDay from "../components/HomePageComponents/CardWordOfTheDay";
 import Logo from "../components/HomePageComponents/Logo";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-    return (
-        <Wrapper>
-            <Logo />
-            <CardWordOfTheDay />
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Logo />
+      <Headline>Wort des Tages</Headline>
+      <CardWordOfTheDay />
+      <GamesList>
+        <Headline>Sprachspiele</Headline>
+        <button><Link to="/games/wordflow">Wordflow spielen</Link></button>
+      </GamesList>
+    </Wrapper>
+  );
 }
 
-const Wrapper = styled.body`
+const Wrapper = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -19,3 +25,20 @@ const Wrapper = styled.body`
   justify-content: flex-start;
   background-: #ebf4ff;
 `;
+const Headline = styled.h2`
+  font-weight: 400;
+  color: #000;
+  font-size: 22px;
+  margin-top: 14px;
+  margin-bottom: 14px;
+`;
+const GamesList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 45px;
+  width: 345px;
+  height: 300px;
+`;
+
