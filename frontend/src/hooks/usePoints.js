3b98ab7f5f635a16a346e92fa6e6b25c0751373a
bloier <airinaight@gmail.com>
2021-07-08 20:50:19 +0200

    Small styling changes, e.g. button and font colors, positioning on page, size of elements

--- frontend/src/hooks/usePoints.js
+++ frontend/src/hooks/usePoints.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function usePoints() {
-    const [points, setPoints] = useState(0);
+  const [points, setPoints] = useState(0);
 
-    function reset(points) {
-        axios
-            .post(`/api/points/`, { points })
-            .then(() => setPoints(0))
-            .catch((error) => console.error(error.message));
-    }
+  function reset(points) {
+    axios
+      .post(`/api/points/`, { points })
+      .then(() => setPoints(0))
+      .catch((error) => console.error(error.message));
+  }
 
-    return { points, reset };
+  return { points, reset };
 }
